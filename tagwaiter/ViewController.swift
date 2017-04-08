f2f3354a9a6c4661de79522177cf61ff9c4fc3da
Manu <manuellp@MacBook-Pro-de-Manu.local>
2017-04-08 14:09:26 +0200

    Primer Commit

---
+++ tagwaiter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  tagwaiter
+//
+//  Created by Manu on 30/3/17.
+//  Copyright © 2017 Manu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonQR(_ sender: UIButton) {
+        
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
