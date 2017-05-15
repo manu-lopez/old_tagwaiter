5a8968cb63d58249cd55234145859edd7883f1ef
Manu <manuellp@MacBook-Pro-de-Manu.local>
2017-05-15 10:53:50 +0200

    Añadido lector qr, status bar light en pantalla inicio

--- tagwaiter/ViewController.swift
+++ tagwaiter/ViewController.swift
@@ -9,21 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func buttonQR(_ sender: UIButton) {
-        
+    
+    //Set to white status bar
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
     }
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
     }
 
 
