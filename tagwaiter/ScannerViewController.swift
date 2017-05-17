bdeac1491e51e1e4563977a43c017ea21c8ccf6e
Manu <manuellp@MacBook-Pro-de-Manu.local>
2017-05-17 20:55:56 +0200

    Las comprobaciones del QR y la localizacion en la base de datos se ha pasado a ScannerViewController. Cuando detecta qr se activa el boton y hace todas las comprobaciones (localizacion y codigo qr) Se han elimiando los distintos segue, ya no son necesarios

--- tagwaiter/ScannerViewController.swift
+++ tagwaiter/ScannerViewController.swift
@@ -8,11 +8,16 @@
 
 import AVFoundation
 import UIKit
+import CoreLocation //obtener localizacion usuario
+import Alamofire //hacer consultas al servidor
 
-class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, CLLocationManagerDelegate {
     @IBOutlet var topbar: UIView!
     
     @IBOutlet var sendQRButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
     
     var captureSession:AVCaptureSession?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
@@ -36,6 +41,21 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //opciones boton
+        sendQRButton.isEnabled = false
+        sendQRButton.alpha = 0.6
+        sendQRButton.backgroundColor = UIColor.lightGray
+        sendQRButton.setTitle("Escanea un QR", for: .normal)
+
+        
+        //Location
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        currentLocation = locationManager.location
+
         
         // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter.
         let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
@@ -82,17 +102,11 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
                 
             }
             
-            sendQRButton.isEnabled = false
         } catch {
             // If any error occurs, simply print it out and don't continue any more.
             print(error)
             return
         }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
@@ -117,7 +131,9 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
             qrCodeFrameView?.frame = barCodeObject!.bounds
             
             if metadataObj.stringValue != nil {
-                qrCode = metadataObj.stringValue
+                qrCode = metadataObj.stringValue!
+//                verifyConnection(code: metadataObj.stringValue!, latitud: currentLocation.coordinate.latitude, longitud: currentLocation.coordinate.longitude)
+                
                 sendQRButton.isEnabled = true
                 sendQRButton.backgroundColor = UIColor.green
                 sendQRButton.setTitle("Escanear", for: .normal)
@@ -126,22 +142,63 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         }
     }
     
-    
-    @IBAction func sendQR(_ sender: Any) {
-        performSegue(withIdentifier: "passQrCode", sender: qrCode)
+    @IBAction func CloseScanner(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
-    //Pass code to ViewController
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    @IBAction func sendQR(_ sender: Any) {
         
-        if segue.identifier == "passQrCode"{
+        sendQRButton.setTitle("Escaneando...", for: .normal)
+        verifyConnection(code: qrCode!, latitud: currentLocation.coordinate.latitude, longitud: currentLocation.coordinate.longitude)
+    }
+    
+    func verifyConnection(code: String, latitud: Double, longitud: Double)
+    {
+        
+        Alamofire.request("http://api.disainin.com/foodtags/1/access/qr/\(code)/\(latitud)/\(longitud)").responseJSON { response in
             
-            if let destination = segue.destination as? ViewController {
-                destination.qrCode = qrCode
-                destination.haveQrCode = true
-                self.dismiss(animated: true)
+            if let JSON = response.result.value as? [String: Any] {
+                if let name = JSON["name"] as? String {
+                    UserDefaults.standard.set(name, forKey: "shopName")
+                }
+                if let sessionId = JSON["sessionId"] as? String {
+                    UserDefaults.standard.set(sessionId, forKey: "sessionId")
+                }
+                if let shopId = JSON["shopId"] as? String {
+                    UserDefaults.standard.set(shopId, forKey: "shopId")
+                }
+                if let token = JSON["token"] as? String {
+                    UserDefaults.standard.set(token, forKey: "token")
+                }
+                if let valid = JSON["valid"] as? Bool {
+                    
+                    UserDefaults.standard.set(valid, forKey: "valid")
+                    
+                    if valid == true {
+                        
+                        let yes = UIAlertController(title: "Has conectado", message: "Al fin tenemos disco duro!", preferredStyle: .alert)
+                        yes.addAction(UIAlertAction(title: "Dale", style: .default, handler: self.novalid))
+                        
+                        self.present(yes, animated: true, completion: nil)
+                        
+                    } else {
+                        
+                        let alert = UIAlertController(title: "No se ha podido conecctar", message: "No se ha podido conectar, puede que la mesa esté ocupada", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Probar de nuevo", style: .default, handler: self.novalid))
+                        
+                        self.present(alert, animated: true, completion: nil)
+
+
+                    }
+                }
             }
         }
-    
+    }
+
+    //handler para alert
+    func novalid(action: UIAlertAction){
+//        dismiss(animated: true, completion: nil)
+        self.viewDidLoad()
     }
 }
--- tagwaiter/ViewController.swift
+++ tagwaiter/ViewController.swift
@@ -7,89 +7,24 @@
 //
 
 import UIKit
-import CoreLocation //obtener localizacion usuario
-import Alamofire //hacer consultas al servidor
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
-    var qrCode: String?
-    var haveQrCode = false
-    let locationManager = CLLocationManager()
-    var currentLocation: CLLocation!
     
-    @IBAction func buttonQR(_ sender: UIButton) {
-        
-    }
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        currentLocation = locationManager.location
-        
-        if haveQrCode == true {
-            var isValid = verifyConnection(code: qrCode!, latitud: currentLocation.coordinate.latitude, longitud: currentLocation.coordinate.longitude)
-            
-            print("isValid: \(isValid)")
-            if isValid == true
-            {
-                print("Correcto")
-            } else if isValid == false{
-                
-                let alert = UIAlertController(title: "No se ha podido conecctar", message: "No se ha podido conectar, puede que la mesa esté ocupada", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Probar de nuevo", style: .default, handler: nil))
-                
-                present(alert, animated: true, completion: nil)
-                
-            }
-        }
-    }
-    
-    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue)
-    {
-        dismiss(animated: true, completion: nil)
-    }
     
     //Set to white status bar
     override var preferredStatusBarStyle: UIStatusBarStyle
     {
         return .lightContent
     }
+
     
-    
-    func verifyConnection(code: String, latitud: Double, longitud: Double) -> Bool
-    {
-        var conn = false
-        
-        Alamofire.request("http://api.disainin.com/foodtags/1/access/qr/\(code)/\(latitud)/\(longitud)").responseJSON { response in
-            
-            if let JSON = response.result.value as? [String: Any] {
-                if let name = JSON["name"] as? String {
-                    UserDefaults.standard.set(name, forKey: "shopName")
-                }
-                if let sessionId = JSON["sessionId"] as? String {
-                    UserDefaults.standard.set(sessionId, forKey: "sessionId")
-                }
-                if let shopId = JSON["shopId"] as? String {
-                    UserDefaults.standard.set(shopId, forKey: "shopId")
-                }
-                if let token = JSON["token"] as? String {
-                    UserDefaults.standard.set(token, forKey: "token")
-                }
-                if let valid = JSON["valid"] as? Bool {
-                    print("Valid: \(valid)")
-                    UserDefaults.standard.set(valid, forKey: "valid")
-                    if valid == true {conn = true}
-                }
-            }
-        }
-        print("Conn: \(conn)")
-        return conn
+    @IBAction func buttonQR(_ sender: UIButton) {
     }
     
-}
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        }
+    }
 
